10d8668b400dbd73851e3a7f679ecfdc63cd0f31
MaurineDuray <maurine.duray@skynet.be>
2021-03-23 19:53:22 +0100

    sansJS

--- index.php
+++ index.php
@@ -8,57 +8,11 @@
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
     <link rel="stylesheet" href="style.css" type="text/css">
-    <script>
-        window.addEventListener("load",()=>{
-            const burger=document.querySelector(".menuBurger")
-            const menu=document.querySelector("#slideMenu")
-            console.log("menu")
-            burger.addEventListener("click",()=>{
-                menu.classList.toggle("menuON");
-            })
-
-            const croix = document.querySelector(".croix")
-            croix.addEventListener("click",()=>{
-                menu.classList.remove("menuON");
-            })
-
-            const linksh = document.querySelectorAll(".haut-titre")
-            const links = document.querySelectorAll(".sous-titre")
-           
-            linksh.forEach(link => {
-                link.addEventListener('click',()=>{
-                    menu.classList.remove("menuON");
-                })
-            })
-        
-            links.forEach(link => {
-                link.addEventListener('click',()=>{
-                    menu.classList.remove("menuON");
-                })
-            })
-
-            /* Menu tri dans portfolio */
-            const fermer =document.querySelector(".fermer")
-            const menuTrvx = document.querySelector(".menuTrvxResp")
-            const menuTri = document.querySelector(".menutrier")
-
-            menuTri.addEventListener("click",()=>{
-                menuTrvx.classList.add("menuOpen")
-            })
-            fermer.addEventListener("click",()=>{
-                menuTrvx.classList.remove("menuOpen")
-            })
-
-            const liTri = document.querySelector(".menuTrvxResp ul li ")
-            liTri.addEventListener("click",()=>{
-                menuTrvx.classList.remove("menuOpen")
-            })
-        })
-    </script>
+    
 </head>
 <body>
-    <nav id="normal">
-            <div class="logo"><a href="index.php"><img src="images/logoMenu.png" alt=""></a></div>
+    <nav>
+            <div class="logo"><a href="index.html"><img src="images/logoMenu.png" alt=""></a></div>
             <ul>
                     <li><a href="index.php"><div class="haut-titre">Welcome</div><div class="sous-titre">ACCUEIL</div></a></li>
                     <li><a href="#slide2"><div class="haut-titre">About me</div><div class="sous-titre">À PROPOS</div></a></li>
@@ -72,8 +26,8 @@
                 <div class="barre"></div>
                 <div class="barre"></div>
             </div>
+        
     </nav>
-
     <div class="slide" id="slideMenu">
         <div class="haut">
             <div class="logoResp"><img src="images/logo.png" alt="logo"></div>
@@ -82,24 +36,21 @@
                 <div class="barre" id="barre2"></div>
             </div>
         </div>
-        <nav id="responsive">
-                <ul class="menuResp">
-                <li>
-                    <a href="index.php"><div class="haut-titre">Welcome</div><div class="sous-titre">ACCUEIL</div></a>
-                </li>
-                <li>
-                    <a href="#slide2"><div class="haut-titre">About me</div><div class="sous-titre">À PROPOS</div></a>
-                </li>
-                <li>
-                    <a href="#slide3"><div class="haut-titre">See my work</div><div class="sous-titre">PORTFOLIO</div></a>
-                </li>
-                <li>
-                    <a href="#slide4"><div class="haut-titre">Get in touch</div><div class="sous-titre">CONTACT</div></a>
-                </li>
-                </ul>
-        </div>
-    </nav>
-
+        <ul class="menuResp">
+            <li>
+                <a href="index.php"><div class="haut-titre">Welcome</div><div class="sous-titre">ACCUEIL</div></a>
+            </li>
+            <li>
+                <a href="#slide2"><div class="haut-titre">About me</div><div class="sous-titre">À PROPOS</div></a>
+            </li>
+            <li>
+                <a href="#slide3"><div class="haut-titre">See my work</div><div class="sous-titre">PORTFOLIO</div></a>
+            </li>
+            <li>
+                <a href="#slide4"><div class="haut-titre">Get in touch</div><div class="sous-titre">CONTACT</div></a>
+            </li>
+        </ul>
+    </div>
     <div class="slide" id="slide1">
         
         <div class="row g-0">
@@ -118,7 +69,6 @@
         </div>
         
     </div>
-
     <div class="slide" id="slide2">
         <div class="container">
         <div class="row">
@@ -161,12 +111,10 @@
                     <h6>Passions</h6>
                         <div class="trait"></div>
                         <p>Nature/animaux - dessin - cuisine - balades - musique </p>
-
             </div>
         </div>
     </div>
     </div>
-
     <div class="slide" id="slide3">
         <div class="menuTrvxResp">
             <div class="fermer">
@@ -175,12 +123,12 @@
             </div>
             <div class="fleurslide3"><img src="images/fleurVerte.png" alt=""></div>
             <ul>
-                <li ><a href="#slide3">Dessin et illustration</a></li>
-                <li ><a href="#slide3">Retouches graphiques  </a></li>
-                <li ><a href="#slide3">Dessin vectoriel</a></li>
-                <li ><a href="#slide3">Mise en page PAO</a></li>
-                <li ><a href="#slide3">Web - WebDesign</a></li>
-                <li ><a href="#slide3">Animation</a></li>  
+                <li class="col-md-2 "><a href="#slide3">Dessin et illustration</a></li>
+                <li class="col-md-2"><a href="#slide3">Retouches graphiques  </a></li>
+                <li class="col-md-2"><a href="#slide3">Dessin vectoriel</a></li>
+                <li class="col-md-2"><a href="#slide3">Mise en page PAO</a></li>
+                <li class="col-md-2"><a href="#slide3">Web - WebDesign</a></li>
+                <li class="col-md-2"><a href="#slide3">Animation</a></li>  
             </ul>
         </div>
         <div class="fleurslide3"><img src="images/fleurVerte.png" alt=""></div>
@@ -219,7 +167,6 @@
                     <p>Trier</p>
                 </div>
         </div>
-
             <div class="container overflow-hidden">
                 <div class="row g-3">
                     <div class="col-md-4">
@@ -243,13 +190,11 @@
                 </div>
                 <button type="button" id="ensav" class="btn btn-primary">En voir plus</button>
               </div>
-
            
         
     </div>
     </div>
    
-
     <div class="slide" id="slide4">
         <div class="row">
             <div class="gauche4 col-lg-6">
@@ -277,7 +222,6 @@
             </div>
         </div>
     </div>
-
     <footer>
         <div class="container">
         <div class="row">
@@ -311,6 +255,5 @@
             <div class="copy col-md-6">Copyright @ Duray Maurine</div>
         </div>
     </footer>
-
 </body>
-</html>
+</html> 
